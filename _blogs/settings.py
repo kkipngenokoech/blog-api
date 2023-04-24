a2592a6dd111d50c86ff514c51a89bca8008685d
kip <kkipngenokoech22@gmail.com>
2023-04-24 19:57:01 +0300

    app accounts setup

--- _blogs/settings.py
+++ _blogs/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # local apps
+    'accounts',
+    # third party apps
+
+    # inbuilt applications
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

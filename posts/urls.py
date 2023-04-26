288b4016cb4866423e61277b8be0e6b9e36f16f4
kip <kkipngenokoech22@gmail.com>
2023-04-26 15:13:35 +0300

    updated the necessary required fields to title body an id only

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, PostDetailView
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index")
+    path("", IndexView.as_view(), name="index"),
+    path("<int:pk>/",PostDetailView.as_view(), name="postdetail")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Post
 from .serializers import PostSerializer
 # Create your views here.
 class IndexView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailView(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
